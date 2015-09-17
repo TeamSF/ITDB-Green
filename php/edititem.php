6d911129e7b0667eb5ea69b6a3cecbf426f70d40
Chefkeks <chefkeks@googlemail.com>
2015-09-17 15:18:29 +0200

    Implemented action log functionality, only associations are still missing

--- php/edititem.php
+++ php/edititem.php
@@ -11,6 +11,13 @@ $formvars=array("itemtypeid","function","manufacturerid","label",
   "cpu","cpuno","corespercpu", "ram", "rackmountable", "rackid","rackposition","rackposdepth","usize","status",
   "macs","ipv4","ipv6","remadmip","panelport","switchid","switchport","ports");
 
+//Log settings for actions
+$sql="SELECT uselog,log_actions FROM settings";
+$sth=$dbh->query($sql);
+$r=$sth->fetch(PDO::FETCH_ASSOC);
+$log=$r['log_actions'];
+$uselog=$r['uselog'];
+
 /* delete item */
 if (isset($_GET['delid'])) { 
   //first handle file associations
@@ -127,63 +134,247 @@ if (isset($_POST['itemtypeid']) && ($_GET['id']!="new") && isvalidfrm()) {
   if (!isset($_POST['itlnk'])) $itlnk=array();
   if (!isset($_POST['invlnk'])) $invlnk=array();
 
+if($uselog) {
+// Get current info of item
+  $sql="SELECT sn,sn2,sn3,dnsname,macs,ipv4,ipv6,rackposition,rackposdepth FROM items where items.id='$id'";
+  $sth=db_execute($dbh,$sql);
+  $curriteminfo=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $curriteminfo=$curriteminfo[0];
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX BEGINN TODO XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-// TODO:
-// - Aus Settings den Wert für log auslesen
-// - SQL-Queries bauen zum Vergleich zwischen AlterWert (aus Datenbank) und NeuerWert (mittels $_POST[Variablenname])
-// - SQL Insert für ausgewählte log-settings durchführen
-// - Meldungen einheitlich halten:
-//      "Item added by $_COOKIE['itdbuser']"
-//      "Hostname "NeuerWert" added by $_COOKIE['itdbuser']
-//      "Hostname updated from "AlterWert" to "NeuerWert" by $_COOKIE['itdbuser']
-//      "Hostname "AlterWert" removed by $_COOKIE['itdbuser']
-// - Generische Meldung "Updated by $_COOKIE['itdbuser']" (Zeilen 165 bis 181) entfernen (retire it)
-// - Eventuell manche SQL-Queries auslagern in model.php oder functions.php
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ENDE TODO XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+  $currsn=$curriteminfo['sn'];
+  $currsn2=$curriteminfo['sn2'];
+  $currsn3=$curriteminfo['sn3'];
+  $currdnsname=$curriteminfo['dnsname'];
+  $currmacs=$curriteminfo['macs'];
+  $curripv4=$curriteminfo['ipv4'];
+  $curripv6=$curriteminfo['ipv6'];
+  $currrackposition=$curriteminfo['rackposition'];
+  $currrackposdepth=$curriteminfo['rackposdepth'];
 
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX BEGINN LOG/JOURNAL TEIL XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// Get new info of item
+  $newsn=$_POST['sn'];
+  $newsn2=$_POST['sn2'];
+  $newsn3=$_POST['sn3'];
+  $newdnsname=$_POST['dnsname'];
+  $newmacs=$_POST['macs'];
+  $newipv4=$_POST['ipv4'];
+  $newipv6=$_POST['ipv6'];
+  $newrackposition=$_POST['rackposition'];
+  $newrackposdepth=$_POST['rackposdepth'];
 
+// Get current user of item
   $sql="SELECT items.userid,users.username from users,items where userid=users.id and items.id='$id'";
   $sth=db_execute($dbh,$sql);
   $curruser=$sth->fetchAll(PDO::FETCH_ASSOC);
   $curruser=$curruser[0];
 
+// Get new user of item
   $sql="SELECT username from users where id=$userid";
   $sth=db_execute($dbh,$sql);
   $newuser=$sth->fetchAll(PDO::FETCH_ASSOC);
   $newuser=$newuser[0];
 
-  if ($userid!=$curruser['userid']) { //changed user
-    $str="Updated user from {$curruser['username']} to {$newuser['username']}";
+// Get current status of item
+  $sql="SELECT items.status,statustypes.statusdesc from statustypes,items where status=statustypes.id and items.id='$id'";
+  $sth=db_execute($dbh,$sql);
+  $currstatus=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $currstatus=$currstatus[0];
+
+// Get new status of item
+  $sql="SELECT id,statusdesc from statustypes where id=$status";
+  $sth=db_execute($dbh,$sql);
+  $newstatus=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $newstatus=$newstatus[0];
+
+// Get current location of item
+  $sql="SELECT items.locationid,locations.name,locations.floor from locations,items where locationid=locations.id and items.id='$id'";
+  $sth=db_execute($dbh,$sql);
+  $currlocation=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $currlocation=$currlocation[0];
+  $currlocationid=$currlocation['locationid'];
+  $currlocationname=$currlocation['name'];
+  $currlocationfloor="Floor ".$currlocation['floor'];
+
+//Check only if not empty to avoid SQL errors
+if(!empty($locationid))
+{
+// Get new location of item
+  $sql="SELECT id,name,floor from locations where id=$locationid";
+  $sth=db_execute($dbh,$sql);
+  $newlocation=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $newlocation=$newlocation[0];
+  $newlocationid=$newlocation['id'];
+  $newlocationname=$newlocation['name'];
+  $newlocationfloor="Floor ".$newlocation['floor'];
+}
+else
+{
+  $newlocation = $newlocationid = $newlocationname = $newlocationfloor= "";
+}
+
+// Get current location and area of item
+  $sql="SELECT items.locationid,items.locareaid,locations.name,locations.floor,locareas.areaname from locations,locareas,items where items.locationid=locations.id and locareaid=locareas.id and items.locationid=locareas.locationid and items.id='$id'";
+  $sth=db_execute($dbh,$sql);
+  $currlocationarea=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $currlocationarea=$currlocationarea[0];
+  $currlocationarealocationid=$currlocationarea['locationid'];
+  $currlocationarealocareaid=$currlocationarea['locareaid'];
+  $currlocationareaareaname=$currlocationarea['areaname'];
+
+//Check only if not empty to avoid SQL errors
+if(!empty($locareaid))
+{
+// Get new location area of item
+  $sql="SELECT id,areaname from locareas where locareas.id=$locareaid and locareas.locationid=$newlocationid";
+  $sth=db_execute($dbh,$sql);
+  $newlocationarea=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $newlocationarea=$newlocationarea[0];
+  $newlocationareaid=$newlocationarea['id'];
+  $newlocationareaareaname=$newlocationarea['areaname'];
+}
+else
+{
+  $newlocationarea = $newlocationareaid = $newlocationareaareaname = "";
+}
+
+// Get current rack of item
+  $sql="SELECT items.rackid,racks.id,racks.label,racks.model,racks.usize from racks,items where items.rackid=racks.id and items.id='$id'";
+  $sth=db_execute($dbh,$sql);
+  $currrack=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $currrack=$currrack[0];
+  $currrackid=$currrack['id'];
+  $currracklabel=$currrack['label'];
+  $currrackmodel=$currrack['model'];
+  $currrackusize=$currrack['usize']."U";
+  $currrackname=$currracklabel.", ".$currrackusize." ".$currrackmodel;
+
+if(!empty($rackid))
+{
+// Get new rack of item
+  $sql="SELECT id,label,model,usize from racks where id=$rackid";
+  $sth=db_execute($dbh,$sql);
+  $newrack=$sth->fetchAll(PDO::FETCH_ASSOC);
+  $newrack=$newrack[0];
+  $newrackid=$newrack['id'];
+  $newracklabel=$newrack['label'];
+  $newrackmodel=$newrack['model'];
+  $newrackusize=$newrack['usize']."U";
+  $newrackname=$newracklabel.", ".$newrackusize." ".$newrackmodel;
+  print_r($newrack);
+}
+else
+{
+  $newrack = $newrackid = $newracklabel = $newrackusize = $newrackname = "";
+}
+
+
+$actions_entry=array();
+// changed user
+if ($log&2 && $userid!=$curruser['userid']) $actions_entry[]="Updated user from {$curruser['username']} to {$newuser['username']}";
+
+// changed status
+if ($log&4 && $newstatus['id']!=$currstatus['status']) $actions_entry[]="Updated status from {$currstatus['statusdesc']} to {$newstatus['statusdesc']}";
+
+if($log&8) { // added, removed or changed serial 1
+    if($newsn!=$currsn && empty($currsn) && !empty($newsn)) $actions_entry[]="Added S/N {$newsn}";
+    elseif($newsn!=$currsn && !empty($currsn) && empty($newsn)) $actions_entry[]="Removed S/N {$currsn}";
+    elseif($newsn!=$currsn) $actions_entry[]="Updated S/N from {$currsn} to {$newsn}";
+
+    // added, removed or changed serial 2
+    if($newsn2!=$currsn2 && empty($currsn2) && !empty($newsn2)) $actions_entry[]="Added S/N 2 {$newsn2}";
+    elseif($newsn2!=$currsn2 && !empty($currsn2) && empty($newsn2)) $actions_entry[]="Removed S/N 2 {$currsn2}";
+    elseif($newsn2!=$currsn2) $actions_entry[]="Updated S/N 2 from {$currsn2} to {$newsn2}";
+
+    // added, removed or changed serial 3
+    if($newsn3!=$currsn3 && empty($currsn3) && !empty($newsn3)) $actions_entry[]="Added S/N 3 {$newsn3}";
+    elseif($newsn3!=$currsn3 && !empty($currsn3) && empty($newsn3)) $actions_entry[]="Removed S/N 3 {$currsn3}";
+    elseif($newsn3!=$currsn3) $actions_entry[]="Updated S/N 3 from {$currsn3} to {$newsn3}";
+}
+
+if ($log&16) { //added, removed or changed location or area/room
+    if($newlocationid!=$currlocationid && empty($currlocationid) && !empty($newlocationid)) $actions_entry[]="Added location {$newlocationname}, {$newlocationfloor}";
+    elseif($newlocationid!=$currlocationid && !empty($currlocationid) && empty($newlocationid)) $actions_entry[]="Removed location {$currlocationname}, {$currlocationfloor}";
+    elseif($newlocationid!=$currlocationid && $newlocationname==$currlocationname) $actions_entry[]="Updated location from {$currlocationname}, {$currlocationfloor} to {$newlocationfloor}";
+    elseif($newlocationid!=$currlocationid && $newlocationname!=$currlocationname) $actions_entry[]="Updated location from {$currlocationname}, {$currlocationfloor} to {$newlocationname}, {$newlocationfloor}";
+
+    if($newlocationareaid!=$currlocationarealocareaid && empty($currlocationarealocareaid) && !empty($newlocationareaid)) $actions_entry[]="Added area/room {$newlocationareaareaname}";
+    elseif($newlocationareaid!=$currlocationarealocareaid && !empty($currlocationarealocareaid) && empty($newlocationareaid)) $actions_entry[]="Removed area/room {$currlocationareaareaname}";
+    elseif($newlocationareaid!=$currlocationarealocareaid) $actions_entry[]="Updated area/room from {$currlocationareaareaname} to {$newlocationareaareaname}";
+}
+
+if ($log&32) { //added, removed or changed rack or rack position
+    if($newrackid!=$currrackid && empty($currrackid) && !empty($newrackid)) $actions_entry[]="Added rack {$newrackname}";
+    elseif($newrackid!=$currrackid && !empty($currrackid) && empty($newrackid)) $actions_entry[]="Removed rack {$currrackname}";
+    elseif($newrackid!=$currrackid) $actions_entry[]="Updated rack from {$currrackname} to {$newrackname}";
+
+    $currrackposdepthlabel="";
+    if($currrackposdepth == '6') $currrackposdepthlabel=" (FM-)";
+    if($currrackposdepth == '3') $currrackposdepthlabel=" (-MB)";
+    if($currrackposdepth == '4') $currrackposdepthlabel=" (F--)";
+    if($currrackposdepth == '2') $currrackposdepthlabel=" (-M-)";
+    if($currrackposdepth == '1') $currrackposdepthlabel=" (--B)";
+    if($currrackposdepth == '7') $currrackposdepthlabel=" (FMB)";
+
+    $newrackposdepthlabel="";
+    if($newrackposdepth == '6') $newrackposdepthlabel=" (FM-)";
+    if($newrackposdepth == '3') $newrackposdepthlabel=" (-MB)";
+    if($newrackposdepth == '4') $newrackposdepthlabel=" (F--)";
+    if($newrackposdepth == '2') $newrackposdepthlabel=" (-M-)";
+    if($newrackposdepth == '1') $newrackposdepthlabel=" (--B)";
+    if($newrackposdepth == '7') $newrackposdepthlabel=" (FMB)";
+
+    $rack_posdepth_msg="";
+    if($newrackposdepth!=$currrackposdepth && empty($currrackposdepth) && !empty($newrackposdepth)) $rack_posdepth_msg=$newrackposdepthlabel;
+    elseif($newrackposdepth!=$currrackposdepth && !empty($currrackposdepth) && empty($newrackposdepth)) $rack_posdepth_msg=$currrackposdepthlabel;
+    elseif($newrackposdepth==$currrackposdepth && empty($currrackposdepth) && !empty($newrackposdepth)) $rack_posdepth_msg=$newrackposdepthlabel;
+    elseif($newrackposdepth==$currrackposdepth && !empty($currrackposdepth) && empty($newrackposdepth)) $rack_posdepth_msg=$currrackposdepthlabel;
+    elseif($newrackposdepth==$currrackposdepth) $rack_posdepth_msg=$currrackposdepthlabel;
+    elseif($currrackposdepth!=$newrackposdepth) $rack_posdepth_msg=$newrackposdepthlabel;
+
+    if($newrackposition!=$currrackposition && empty($currrackposition) && !empty($newrackposition)) $actions_entry[]="Added rack position {$newrackposition}{$rack_posdepth_msg}";
+    elseif($newrackposition!=$currrackposition && !empty($currrackposition) && empty($newrackposition) && !empty($newrackposdepth)) $actions_entry[]="Removed rack position {$currrackposition}";
+    elseif($newrackposition!=$currrackposition && !empty($currrackposition) && empty($newrackposition) && empty($newrackposdepth)) $actions_entry[]="Removed rack position {$currrackposition}{$currrackposdepthlabel}";
+    elseif($newrackposition!=$currrackposition) $actions_entry[]="Updated rack position from {$currrackposition}{$currrackposdepthlabel} to {$newrackposition}{$newrackposdepthlabel}";
+    elseif($newrackposition==$currrackposition && empty($currrackposdepth) && !empty($newrackposdepth)) $actions_entry[]="Added rack position {$currrackposition} depth {$newrackposdepthlabel}";
+    elseif($newrackposition==$currrackposition && !empty($currrackposdepth) && empty($newrackposdepth)) $actions_entry[]="Removed rack position depth {$currrackposdepthlabel}";
+    elseif($newrackposition==$currrackposition && $newrackposdepth!=$currrackposdepth) $actions_entry[]="Updated rack position {$currrackposition} depth from {$currrackposdepthlabel} to {$newrackposdepthlabel}";
+}
+
+if($log&64) { // added, removed or changed dnsname
+    if($newdnsname!=$currdnsname && empty($currdnsname) && !empty($newdnsname)) $actions_entry[]="Added DNS Name {$newdnsname}";
+    elseif($newdnsname!=$currdnsname && !empty($currdnsname) && empty($newdnsname)) $actions_entry[]="Removed DNS Name {$currdnsname}";
+    elseif($newdnsname!=$currdnsname) $actions_entry[]="Updated DNS Name from {$currdnsname} to {$newdnsname}";
+}
+
+if($log&128) { // added, removed or changed macs
+    if($newmacs!=$currmacs && empty($currmacs) && !empty($newmacs)) $actions_entry[]="Added MACs {$newmacs}";
+    elseif($newmacs!=$currmacs && !empty($currmacs) && empty($newmacs)) $actions_entry[]="Removed MACs {$currmacs}";
+    elseif($newmacs!=$currmacs) $actions_entry[]="Updated MACs from {$currmacs} to {$newmacs}";
+}
+
+if($log&256) { // added, removed or changed ipv4
+    if($newipv4!=$curripv4 && empty($curripv4) && !empty($newipv4)) $actions_entry[]="Added IPv4 {$newipv4}";
+    elseif($newipv4!=$curripv4 && !empty($curripv4) && empty($newipv4)) $actions_entry[]="Removed IPv4 {$curripv4}";
+    elseif($newipv4!=$curripv4) $actions_entry[]="Updated IPv4 from {$curripv4} to {$newipv4}";
+
+    // added, removed or changed ipv6
+    if($newipv6!=$curripv6 && empty($curripv6) && !empty($newipv6)) $actions_entry[]="Added IPv6 {$newipv6}";
+    elseif($newipv6!=$curripv6 && !empty($curripv6) && empty($newipv6)) $actions_entry[]="Removed IPv6 {$curripv6}";
+    elseif ($newipv6!=$curripv6) $actions_entry[]="Updated IPv6 from {$curripv6} to {$newipv6}";
+}
+
+foreach($actions_entry as $m)
+{
     $sql="INSERT into actions (itemid, actiondate,description,invoiceinfo,isauto,entrydate) values ".
-	 "($id,".time().",'$str' , '',1,".time().")";
+	 "($id,".time().",'$m by {$_COOKIE["itdbuser"]}' , '',1,".time().")";
     db_exec($dbh,$sql);
-  }
+}
+
+} //end action log
 
   $sql="UPDATE items set $set WHERE id=$id";
-  db_exec($dbh,$sql); 
+  db_exec($dbh,$sql);
 
-  //Add new action entry
-  //if not exists already for today
-  $sql="SELECT itemid,entrydate,description, isauto FROM actions WHERE itemid='$id' ORDER BY entrydate DESC LIMIT 1";
-  $sth=db_execute($dbh,$sql);
-  $laction=$sth->fetchAll(PDO::FETCH_ASSOC);
-
-  $upstr="Updated by {$_COOKIE["itdbuser"]}";
-  $ldesc=$laction[0]['description'];
-  $ldate=date("Ymd",$laction[0]['entrydate']);
-  $ndate=date("Ymd",time());
-
-
-  if (($upstr != $ldesc) && ($ldate != $ndate) ) {
-    //add new action entry
-    $sql="INSERT into actions (itemid, actiondate,description,invoiceinfo,isauto,entrydate) values ".
-	 "($id,".time().",'$upstr' , '',1,".time().")";
-    db_exec($dbh,$sql);
-//echo "HERE:($upstr,$ldesc), ($ldate,$ndate);";
-  }
-// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ENDE LOG/JOURNAL TEIL XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
   //update item links
   //remove old links for this object
@@ -314,11 +505,13 @@ elseif (isset($_POST['itemtypeid']) && ($_GET['id']=="new")&&isvalidfrm()) {
     db_exec($dbh,$sql);
   }
 
-
+  if($uselog && $log&1)
+  {
   //add new action entry
   $sql="INSERT into actions (itemid, actiondate,description,invoiceinfo,isauto,entrydate) values ".
-       "($lastid,".time().",'Added by {$_COOKIE["itdbuser"]}' , '',1,".time().")";
+       "($lastid,".time().",'Item #$id added by {$_COOKIE["itdbuser"]}' , '',1,".time().")";
   db_exec($dbh,$sql);
+  }
 
   print "\n<br><b>Added item <a href='$scriptname?action=edititem&amp;id=$lastid'>$lastid</a></b><br>\n";
   if ($lastid) echo "<script>window.location='$scriptname?action=edititem&id=$lastid'</script> "; //go to the new item
