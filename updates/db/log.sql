052c549326a5b221fadfe39b72691ea45fd0a26e
Chefkeks <chefkeks@googlemail.com>
2015-10-06 15:32:31 +0200

    Update database file to include new log options

--- updates/db/log.sql
+++ updates/db/log.sql
@@ -1,5 +1,5 @@
 begin transaction;
 alter table settings add column uselog INTEGER DEFAULT 1;
 alter table settings add column log_show_itemdata INTEGER DEFAULT 1;
-alter table settings add column log_actions INTEGER DEFAULT 8191;
+alter table settings add column log_actions INTEGER DEFAULT 65535;
 commit;
